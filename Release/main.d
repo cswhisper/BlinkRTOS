9bb0792a9aab488e44322c4a186ac71ffb88a334
Charles Whatley <charles@cswhatley.com>
2016-04-11 00:27:33 +0100

    Initial commit.

---
+++ Release/main.d
@@ -0,0 +1,37 @@
+main.d: ../main.cpp \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/FreeRTOS.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/FreeRTOSConfig.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/projdefs.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/portable.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/deprecated_definitions.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/../portable/GCC/ATMega328p/portmacro.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/mpu_wrappers.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/task.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/list.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/queue.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/semphr.h \
+ /home/charles/arduino/ArduinoUnoFreeRTOS/include/queue.h
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/FreeRTOS.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/FreeRTOSConfig.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/projdefs.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/portable.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/deprecated_definitions.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/../portable/GCC/ATMega328p/portmacro.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/mpu_wrappers.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/task.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/list.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/queue.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/semphr.h:
+
+/home/charles/arduino/ArduinoUnoFreeRTOS/include/queue.h:
---
+++ Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/charles/arduino/ArduinoUnoFreeRTOS/Release/libArduinoUnoFreeRTOS.a
+
+LIBS :=
+
