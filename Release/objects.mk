3a481949a4eeb112107fe444776ad2e941bcb7cb
Charles Whatley <charles@cswhatley.com>
2016-04-11 23:01:40 +0100

    Removed the Release directory.

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /home/charles/arduino/ArduinoUnoFreeRTOS/Release/libArduinoUnoFreeRTOS.a
-
-LIBS :=
-
